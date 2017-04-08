a8fbb1ac65495e7797432f024b24a10d45ec307f
Riley Flynn <riley@rileyflynn.me>
2017-04-08 18:49:23 -0230

    Added eval command

--- __init__.py
+++ __init__.py
@@ -1,3 +1,5 @@
+import traceback
+
 from Chainmail.Events import CommandSentEvent
 from Chainmail.MessageBuilder import MessageBuilder, Colours
 from Chainmail.Plugin import ChainmailPlugin
@@ -9,6 +11,31 @@ class ChainmailEssentials(ChainmailPlugin):
 
         self.commands = self.wrapper.CommandRegistry.register_command("!commands", "^!commands$", "Lists commands accessible to a user.", self.command_commands)
         self.plugins = self.wrapper.CommandRegistry.register_command("!plugins", "^!plugins$", "Lists all loaded plugins.", self.command_plugins)
+
+        self.eval_usage_message = MessageBuilder(self.wrapper)
+        self.eval_usage_message.add_field("Usage: ", colour=Colours.red, bold=True)
+        self.eval_usage_message.add_field("!exec <code>", colour=Colours.gold)
+
+        self.eval = self.wrapper.CommandRegistry.register_command("!eval", "^!eval (.+)$", "Evaluates Python expressions.", self.command_eval, True)
+        self.eval_usage = self.wrapper.CommandRegistry.register_command("!eval", "^!eval$", "Displays the usage message.", self.command_eval_usage, True)
+
+    def command_eval(self, event: CommandSentEvent):
+        code = event.args[0]
+        try:
+            result = str(eval(code))
+            error = False
+        except:
+            result = traceback.format_exc(1)
+            error = True
+
+        builder = MessageBuilder(self.wrapper)
+        colour = Colours.green if not error else Colours.red
+        builder.add_field("Result: ", colour=Colours.gold)
+        builder.add_field(result, colour=colour)
+        event.player.send_message(builder)
+
+    def command_eval_usage(self, event: CommandSentEvent):
+        event.player.send_message(self.eval_usage_message)
 
     def command_commands(self, event: CommandSentEvent):
         commands = self.wrapper.CommandRegistry.get_accessible_commands(event.player)
